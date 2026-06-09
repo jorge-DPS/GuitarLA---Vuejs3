be268f3487a2687a632c0acd3264389ad5c36d22
unknown <jorgedanielpacosillo@gmail.com>
2026-06-09 00:16:56 -0400

    directivas de vuejs

--- src/data/guitars.js
+++ src/data/guitars.js
@@ -3,7 +3,7 @@ export const db = [
         id: 1,
         nombre: 'Lukather',
         imagen: 'guitarra_01',
-        descripcion: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        descripcion: 'jorge daniel.',
         precio: 299,
     },
     {
